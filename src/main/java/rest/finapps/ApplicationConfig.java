e1b6cebe1cb8bf5721f47e728f42d86831be4b66
Raúl Piracés Alastuey <raul.piraces@gmail.com>
2015-11-06 23:50:24 +0100

    Added RESTful server logic.

--- src/main/java/rest/finapps/ApplicationConfig.java
+++ src/main/java/rest/finapps/ApplicationConfig.java
@@ -1,7 +1,6 @@
 package rest.finapps;
 
 import org.eclipse.persistence.jaxb.rs.MOXyJsonProvider;
-import org.glassfish.hk2.utilities.binding.AbstractBinder;
 import org.glassfish.jersey.server.ResourceConfig;
 
 /**
@@ -10,27 +9,17 @@ import org.glassfish.jersey.server.ResourceConfig;
  */
 public class ApplicationConfig extends ResourceConfig {
 
-	/**
-     * Default constructor
-     */
-    public ApplicationConfig() {
-    	this(new AddressBook());
-    }
+
 
 
     /**
      * Main constructor
-     * @param addressBook a provided address book
+     * @param a provided address book
      */
-    public ApplicationConfig(final AddressBook addressBook) {
-    	register(AddressBookService.class);
+    public ApplicationConfig() {
+    	register(ArticleService.class);
     	register(MOXyJsonProvider.class);
-    	register(new AbstractBinder() {
 
-			@Override
-			protected void configure() {
-				bind(addressBook).to(AddressBook.class);
-			}});
 	}	
 
 }
