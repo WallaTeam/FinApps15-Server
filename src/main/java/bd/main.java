30ab60abd23f5befebd23614a8e0fe070c97102f
Sergio Soro <ssm.fuentes@gmail.com>
2015-11-06 20:27:22 +0100

    secont commit :)

---
+++ src/main/java/bd/main.java
@@ -0,0 +1,18 @@
+package bd;
+
+/**
+ * Created by teruyi on 6/11/15.
+ */
+public class main {
+
+    public static void main(String [ ] args){
+
+        Database d = new Database();
+
+        System.out.println(d.connect());
+
+
+
+
+    }
+}
